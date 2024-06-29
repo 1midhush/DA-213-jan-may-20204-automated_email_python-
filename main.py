76b2ee73ddcc1c77a20969f3b739b59de82c0446
sirigudi midhush <156108094+1midhush@users.noreply.github.com>
2024-06-29 10:14:23 +0530

    Update main.py

--- main.py
+++ main.py
@@ -15,19 +15,16 @@ SHEET_ID = "1XuHmlXTpq9KpIUguHRbYyTVS342Zer9SkVHXmtUtYWk"
 SHEET_NAME = "Sheet1"
 URL = f"https://docs.google.com/spreadsheets/d/{SHEET_ID}/gviz/tq?tqx=out:csv&sheet={SHEET_NAME}"
 
-
 def load_df(url):
     df = pd.read_csv(url)
     return df
 
 def query_data_and_send_emails(df):
-    
     email_counter = 0
-    for _,row in df.iterrows():
-        if(row["greater_than_75_percent"] == "no"):
+    for _, row in df.iterrows():
+        if row["greater_than_75_percent"] == "no":
             send_email(
-                subject = f'Attendance Remainder (Roll No:{row["Roll_No"]})',
-                
+                subject = f'Attendance Reminder (Roll No:{row["Roll_No"]})',
                 name = row["name"],
                 receiver_email=row["email"],
                 percent=row["percentage_of_attendance"],
@@ -37,27 +34,24 @@ def query_data_and_send_emails(df):
             email_counter += 1
     return f"Total Emails Sent: {email_counter}"        
 
+# Manually sending mail
+# df = load_df(URL)
+# result = query_data_and_send_emails(df)
+# print(result)
 
-
-# manually sending mail
-"""df = load_df(URL)
-result = query_data_and_send_emails(df)
-print(result)
-"""
-
-# using schedule library
+# Using schedule library
 def job_function():
     print(f"Executing job_function at {datetime.now()}")
     df = load_df(URL)
     result = query_data_and_send_emails(df)
     print(result)
     print("Email sending completed........")
-    print("Press any key to countinue..")
+    print("Press any key to continue..")
 
 print("Scheduling started......")
-scheduler = BackgroundScheduler(timezone = 'Asia/Kolkata')
+scheduler = BackgroundScheduler(timezone='Asia/Kolkata')
 
-scheduler.add_job(job_function,'cron',hour=18,minute=40)
+scheduler.add_job(job_function, 'cron', hour=18, minute=40)
 
 scheduler.start()
 
@@ -67,30 +61,18 @@ try:
 except KeyboardInterrupt:
     print("Scheduler stopped manually")    
 
-
-
-# BY Flask server
+# By Flask server
 """
 from flask import Flask
  
-# Flask constructor takes the name of 
-# current module (__name__) as argument.
 app = Flask(__name__)
- 
-# The route() function of the Flask class is a decorator, 
-# which tells the application which URL should call 
-# the associated function.
+
 @app.route('/')
-# ‘/’ URL is bound with hello_world() function.
 def hello_world():
     df = load_df(URL)
     result = query_data_and_send_emails(df)
     return result
  
-# main driver function
 if __name__ == '__main__':
- 
-    # run() method of Flask class runs the application 
-    # on the local development server.
     app.run()
 """
